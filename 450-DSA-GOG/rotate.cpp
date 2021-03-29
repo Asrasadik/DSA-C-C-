a46f5f5cca6dff4ba4e304b18de2c2589392edab
Asrasadik <47492644+Asrasadik@users.noreply.github.com>
2021-03-29 23:45:08 +0530

    rotate an array

--- 450-DSA-GOG/rotate.cpp
+++ 450-DSA-GOG/rotate.cpp
@@ -3,17 +3,21 @@ using namespace std;
 
 void rotate(int a[],int n){
 	int i;
-	swap(a[0],a[n-1]);
 	
-	for(i=0;i<n;i++){
-		cout<<a[i]<<" ";
+	int x=a[n-1];
+	
+	for(i=n-1;i>0;i--){
+		a[i]=a[i-1];
 	}
+	a[0]=x;
+	
+	for(i=0;i<n;i++)
+	cout<<a[i]<<" ";
 	
 }
 
 int main(){
 	int a[]={1,2,3,4,5};
-	int b[]={1,2,3};
 	int n=5;
 	
 	rotate(a,n);
