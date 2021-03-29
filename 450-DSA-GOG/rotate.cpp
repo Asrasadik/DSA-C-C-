91c1a8e193601e674f0daf4fbaebea3a727cbaa4
Asrasadik <47492644+Asrasadik@users.noreply.github.com>
2021-03-29 23:47:28 +0530

    rotate an array k times

--- 450-DSA-GOG/rotate.cpp
+++ 450-DSA-GOG/rotate.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 using namespace std;
 
-void rotate(int a[],int n){
+void rotate(int a[],int n,int k){
 	int i;
-	
+	if(k>0){
 	int x=a[n-1];
 	
 	for(i=n-1;i>0;i--){
@@ -13,13 +13,17 @@ void rotate(int a[],int n){
 	
 	for(i=0;i<n;i++)
 	cout<<a[i]<<" ";
-	
+	cout<<"\n";
+	rotate(a,n,k-1);
+}
+
 }
 
 int main(){
 	int a[]={1,2,3,4,5};
 	int n=5;
+	int k=2;
 	
-	rotate(a,n);
+	rotate(a,n,k);
 	return 0;
 }
